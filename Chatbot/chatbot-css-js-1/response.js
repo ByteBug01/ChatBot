0eb1a2d160c2c5ae3d03f6370f86b5df4bddb81f
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2023-04-10 23:15:24 +0530

    Add files via upload

---
+++ Chatbot/chatbot-css-js-1/response.js
@@ -0,0 +1,11 @@
+const responseObj = {
+  hello: "Hey ! How are you doing ?",
+  hey: "Hey! What's Up",
+  today: new Date().toDateString(),
+  time: new Date().toLocaleTimeString(),
+  hi: "hi, nice to meet you!",
+  coffee:"ofcourse, but i am a bot",
+  thankyou:"Your're welcome!",
+  name:"I am ChatBox!",
+  good:"Pleasure to hear!"
+};
